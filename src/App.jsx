8807394bcc49d0a6e8f3e717f7d854cca9c0f21b
divy-03 <127951881+divy-03@users.noreply.github.com>
2024-08-19 09:59:43 +0530

    added landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import "./styles/app.scss";
+import Navbar from "./components/Navbar";
+import Landing from "./components/Landing";
 
 const App = () => {
-  return <div className="landCont">App</div>;
+  return (
+    <div>
+      <Navbar />
+      <Landing />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Landing.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { MdArrowOutward } from "react-icons/md";
+
+const Landing = () => {
+  return (
+    <div className="landing">
+      <div className="landMain">
+        {["we create", "eye-opening", "presentations"].map((item, idx) => (
+          <div className="masker">
+            {idx==1 && <div></div>}
+            <h2 key={idx}>{item}</h2>
+          </div>
+        ))}
+      </div>
+      <div className="lTxt">
+        <p>For public and private companies</p>
+        <p>From the first pitch to IPO</p>
+        <a href="#">
+          <button>start the project</button>
+          <MdArrowOutward />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
